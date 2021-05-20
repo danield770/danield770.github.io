4325d83126ab48600f3269227275ef0541b47a7f
danield770 <dddavid2@gmail.com>
2021-05-20 07:04:48 +0300

    addded env file for firebase data

--- youtube-playlist/src/database/firebase.js
+++ youtube-playlist/src/database/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyDvVIW5etv1GDntXyTCTeHgmRls6dx7cHY',
-  authDomain: 'playlist-a1c78.firebaseapp.com',
-  projectId: 'playlist-a1c78',
-  storageBucket: 'playlist-a1c78.appspot.com',
-  messagingSenderId: '406422948948',
-  appId: '1:406422948948:web:208d252e5bcc59402571c1',
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_MESSAGING_APP_ID,
 });
 
 const firestore = firebase.firestore();
